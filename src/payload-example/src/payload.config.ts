36f7450b29ec8caedfcdbcfe05785b362f279529
globalworming <andreas.worm@headissue.com>
2025-10-29 13:36:41 +0100

    use postgres

--- src/payload-example/src/payload.config.ts
+++ src/payload-example/src/payload.config.ts
@@ -1,5 +1,5 @@
 // storage-adapter-import-placeholder
-import { sqliteAdapter } from '@payloadcms/db-sqlite'
+import { postgresAdapter } from '@payloadcms/db-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
@@ -28,11 +28,11 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: sqliteAdapter({
-    client: {
-      url: process.env.DATABASE_URI || '',
-    },
-  }),
+  db: postgresAdapter({
+        pool: {
+          connectionString: process.env.DATABASE_URI || '',
+        },
+      }),
   sharp,
   plugins: [
     payloadCloudPlugin(),
